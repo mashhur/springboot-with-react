57303bba77fcdf228841761b2a0039d42efea36e
mik <mik@mik.local>
2018-11-27 09:23:27 +0900

    Spring boot with react boilerplate.

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {};
+
+  componentDidMount() {
+    setInterval(this.hello, 1000);
+}
+
+
+ hello = () => {
+  fetch('/api/time')
+  .then( res => res.text())
+  .then(res => { this.setState( {message: res})
+  });
+};
+    
+ 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">{this.state.message}</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
